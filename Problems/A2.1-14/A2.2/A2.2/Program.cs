cf65352e324d0b0e4efdb43b13d0077b6a8f4c68
YokoShizu <singingsuil@gmail.com>
2023-02-13 09:42:06 +0300

    Add new solutions

---
+++ Problems/A2.1-14/A2.2/A2.2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите двузначное число");
+            double N = Convert.ToInt32(Console.ReadLine());
+            if (N > 9 && N < 100)
+            {
+                double d = Math.Truncate(N / 10);
+                double e = N - d * 10;
+                Console.WriteLine("Число, получаемое при перестановке цифр: " + (e*10 + d));
+            }
+            else
+            {
+                Console.WriteLine("Это не двузначное число!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Problems/A2.1-14/A2.7/A2.7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите трехзначное число");
+            double N = Convert.ToInt32(Console.ReadLine());
+            if (N > 99 && N < 1000)
+            {
+                double s = Math.Truncate(N / 100);
+                double d = Math.Truncate((N - s * 100) / 10);
+                double e = N - s * 100 - d * 10;
+                Console.WriteLine("Число, получающееся при перестановке первой и второй цифр, равно " + (d*100 + s*10 + e));
+            }
+            else
+            {
+                Console.WriteLine("Это не трехзначное число!");
+            }
+            Console.ReadKey();
+        }
+    }
+}

75ae3ff327875ae203f63fa9bb2379d446fa4ae2
YokoShizu <100083604+YokoShizu@users.noreply.github.com>
2024-06-22 01:37:23 +0300

    Delete C/common directory

--- C/common/parser.h
+++
@@ -1,7 +0,0 @@
-#ifndef PARSER_H
-#define PARSER_H
-
-int parser(int flags_q, int *fflag, int templs_q, char *pars_res, int argc,
-           char **argv);
-
-#endif
--- C/common/searchOptions.h
+++
@@ -1,7 +0,0 @@
-#ifndef OPTIONS_H
-#define OPTIONS_H
-
-int searchOptions(int flags_q, int *fflag, int templs_q, char *pars_res,
-                  char **argv);
-
-#endif

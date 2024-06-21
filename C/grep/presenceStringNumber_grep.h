13805e9861281feed25d8eca73d80a010a01dfe9
YokoShizu <100083604+YokoShizu@users.noreply.github.com>
2024-06-22 01:37:43 +0300

    Delete C/grep directory

--- C/grep/presenceStringNumber_grep.h
+++
@@ -1,7 +0,0 @@
-#ifndef NUMBER_OUTPUT_H
-#define NUMBER_OUTPUT_H
-
-void presenceStringNumber_grep(char *templ, char *string, int *quan, int trulse,
-                               int ignor_reg);
-
-#endif

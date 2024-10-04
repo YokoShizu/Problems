387efdd594e8c47558adb88c75c71d29966d628f
YokoShizu <100083604+YokoShizu@users.noreply.github.com>
2024-10-04 21:30:13 +0300

    Delete C++ directory

--- C++/Ex.1.13/Ex.1.13.2/Ex.1.13.2.cpp
+++
@@ -1,8 +0,0 @@
-﻿#include <iostream>
-
-int main()
-{
-	for (int num = 10; num >= 0; --num)
-		std::cout << num << std::endl;
-	return 0;
-}
--- C++/README.md
+++
@@ -1,2 +0,0 @@
-# C++ problems
-Solutions to problems from school and book "C++ Primer" (5th edition)
